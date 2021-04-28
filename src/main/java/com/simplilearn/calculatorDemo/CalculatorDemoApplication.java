1ac57c74f0ec59cad99dda7a395b2340670004ac
student <amit.4.kumar@coforgetech.com>
2021-04-28 15:22:16 +0000

    Add logic and test

---
+++ src/main/java/com/simplilearn/calculatorDemo/CalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.calculatorDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorDemoApplication.class, args);
+	}
+
+}
